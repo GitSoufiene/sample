a2d868bbd83b6994829b75c7bc5eec765bd1fac1
Soufiene Nasri <soufiene.nasri@csdental.com>
2019-01-02 13:40:19 +0100

    Project1 first step

---
+++ Project1/helper/helper.java
@@ -0,0 +1,11 @@
+
+
+public class Helper 
+{
+   
+	void methHelper1()
+	{
+		
+	}
+   
+}
---
+++ Project1/src/main.java
@@ -0,0 +1,15 @@
+public class Project1 
+{
+   public static void main() 
+   {
+	    int x = 0;
+		int y = 1;
+		y++;
+		int z = x + y;
+   }
+   
+   void methAddition(int x, int y)
+   {
+	   return x+y;
+   }
+}
